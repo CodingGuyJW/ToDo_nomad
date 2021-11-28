95ea88c57fca285957f068bb32279787c4ca7c7b
CodingGuyJW <86402538+CodingGuyJW@users.noreply.github.com>
2021-11-28 19:12:42 +0900

    todo-form and todo-list hidden and show

--- js/greeting.js
+++ js/greeting.js
@@ -2,6 +2,8 @@ const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 const logoutBtn = document.querySelector("#log-out");
+const toDoForm = document.getElementById("todo-form");
+const toDoList = document.getElementById("todo-list");
 
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
@@ -18,6 +20,8 @@ function paintGreetings(username) {
     greeting.innerText = `Hello ${username}`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
     logoutBtn.classList.remove(HIDDEN_CLASSNAME);
+    toDoForm.classList.remove(HIDDEN_CLASSNAME);
+    toDoList.classList.remove(HIDDEN_CLASSNAME);
 }
 
 function onLogout(){
@@ -25,7 +29,10 @@ function onLogout(){
     loginForm.classList.remove(HIDDEN_CLASSNAME);
     logoutBtn.classList.add(HIDDEN_CLASSNAME);
     greeting.classList.add(HIDDEN_CLASSNAME);
+    toDoForm.classList.add(HIDDEN_CLASSNAME);
+    toDoList.classList.add(HIDDEN_CLASSNAME);
     loginInput.value = "";
+    location.reload();
 }
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
