f80212a024c58b1a13454db83ca647fc2a92aff3
CodingGuyJW <86402538+CodingGuyJW@users.noreply.github.com>
2021-11-29 11:44:50 +0900

    conclusions

--- js/app.js
+++ js/app.js
@@ -1,9 +1,6 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
-const logoutBtn = document.querySelector("#log-out");
-// const toDoForm = document.getElementById("todo-form");
-// const toDoList = document.getElementById("todo-list");
 
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
@@ -19,20 +16,6 @@ function onLoginSubmit(event) {
 function paintGreetings(username) {
     greeting.innerText = `Hello ${username}`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
-    logoutBtn.classList.remove(HIDDEN_CLASSNAME);
-    // toDoForm.classList.remove(HIDDEN_CLASSNAME);
-    // toDoList.classList.remove(HIDDEN_CLASSNAME);
-}
-
-function onLogout(){
-    localStorage.removeItem(USERNAME_KEY);
-    loginForm.classList.remove(HIDDEN_CLASSNAME);
-    logoutBtn.classList.add(HIDDEN_CLASSNAME);
-    greeting.classList.add(HIDDEN_CLASSNAME);
-    // toDoForm.classList.add(HIDDEN_CLASSNAME);
-    // toDoList.classList.add(HIDDEN_CLASSNAME);
-    loginInput.value = "";
-    // location.reload();
 }
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
@@ -45,5 +28,3 @@ if(savedUsername === null){
     // show the greetings
     paintGreetings(savedUsername);
 }
-
-logoutBtn.addEventListener("click", onLogout);
---
+++ js/quotes.js
@@ -0,0 +1,66 @@
+const quotes = [
+    {
+        quote: "Be yourself; everyone else is already taken.",
+        author: "Oscar Wilde",
+    },
+    {
+        quote: "Be the change that you wish to see in the world.",
+        author: "Mahatma Gandhi",
+    },
+    {
+        quote: "If you tell the truth, you don't have to remember anything.",
+        author: "Mark Twain",
+    },
+    {
+        quote: "Darkness cannot drive out darkness: only light can do that. Hate cannot drive out hate: only love can do that.",
+        author: "Martin Luther King Jr.",
+    },
+    {
+        quote: "Live as if you were to die tomorrow. Learn as if you were to live forever.",
+        author: "Mahatma Gandhi",
+    },
+    {
+        quote: "The fool doth think he is wise, but the wise man knows himself to be a fool.",
+        author: "William Shakespeare",
+    },
+    {
+        quote: "I have not failed. I've just found 10,000 ways that won't work.",
+        author: "Thomas A. Edison",
+    },
+    {
+        quote: "If you judge people, you have no time to love them.",
+        author: "Mother Teresa",
+    },
+    {
+        quote: "Success is not final, failure is not fatal: it is the courage to continue that counts.",
+        author: "Winston S. Churchill",
+    },
+    {
+        quote: "Anyone who has never made a mistake has never tried anything new.",
+        author: "Albert Einstein",
+    },
+    {
+        quote: "Love all, trust a few, do wrong to none.",
+        author: "William Shakespeare",
+    }
+]
+
+const quote = document.querySelector("#quote span:first-child");
+const author = document.querySelector("#quote span:last-child");
+
+const todaysQuote = quotes[ Math.floor( Math.random() * quotes.length) ];
+
+quote.innerText = todaysQuote.quote;
+author.innerText = ` - ${todaysQuote.author}`;
+
+// “Be yourself; everyone else is already taken.” ― Oscar Wilde
+// “Be the change that you wish to see in the world.” ― Mahatma Gandhi
+// “If you tell the truth, you don't have to remember anything.” ― Mark Twain
+// “Darkness cannot drive out darkness: only light can do that. Hate cannot drive out hate: only love can do that.” ― Martin Luther King Jr.
+// “Live as if you were to die tomorrow. Learn as if you were to live forever.” ― Mahatma Gandhi
+// “The fool doth think he is wise, but the wise man knows himself to be a fool.” ― William Shakespeare, As You Like It
+// “I have not failed. I've just found 10,000 ways that won't work.” ― Thomas A. Edison
+// “If you judge people, you have no time to love them.” ― Mother Teresa
+// “Success is not final, failure is not fatal: it is the courage to continue that counts.” ― Winston S. Churchill
+// “Anyone who has never made a mistake has never tried anything new.” ― Albert Einstein
+// “Love all, trust a few, do wrong to none.” ― William Shakespeare
