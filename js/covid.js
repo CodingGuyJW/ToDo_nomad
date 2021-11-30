ee36f4d640c775a8325e2e1c52c4381521bbbbab
CodingGuyJW <86402538+CodingGuyJW@users.noreply.github.com>
2021-11-30 12:27:07 +0900

    mobile-view-quote-margin-top

---
+++ js/covid.js
@@ -0,0 +1,4 @@
+const covid = document.querySelector(".covid");
+const COVID_API_KEY = "UKJsbAvB%2Bm7PJ4EgZmpopHK9VlD6JUHyCdu8B%2BuhBZZj8iL0okDpXxwoHK65a624DSUIQkGUqs1NqBYFP12IvA%3D%3D";
+
+fetch();
