f5779b9d80073cdbcf445da2d21917111973b142
CodingGuyJW <86402538+CodingGuyJW@users.noreply.github.com>
2021-11-30 16:41:07 +0900

    input-text-color-change

--- js/covid.js
+++ js/covid.js
@@ -1,4 +1,16 @@
-const covid = document.querySelector(".covid");
-const COVID_API_KEY = "UKJsbAvB%2Bm7PJ4EgZmpopHK9VlD6JUHyCdu8B%2BuhBZZj8iL0okDpXxwoHK65a624DSUIQkGUqs1NqBYFP12IvA%3D%3D";
+/* Javascript 샘플 코드 */
+var xhr = new XMLHttpRequest();
+var url = 'http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19InfStateJson'; /*URL*/
+var queryParams = '?' + encodeURIComponent('serviceKey') + '='+'UKJsbAvB%2Bm7PJ4EgZmpopHK9VlD6JUHyCdu8B%2BuhBZZj8iL0okDpXxwoHK65a624DSUIQkGUqs1NqBYFP12IvA%3D%3D'; /*Service Key*/
+queryParams += '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent('1'); /**/
+queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('10'); /**/
+queryParams += '&' + encodeURIComponent('startCreateDt') + '=' + encodeURIComponent('20200310'); /**/
+queryParams += '&' + encodeURIComponent('endCreateDt') + '=' + encodeURIComponent('20200315'); /**/
+xhr.open('GET', url + queryParams);
+xhr.onreadystatechange = function () {
+    if (this.readyState == 4) {
+        alert('Status: '+this.status+'nHeaders: '+JSON.stringify(this.getAllResponseHeaders())+'nBody: '+this.responseText);
+    }
+};
 
-fetch();
+xhr.send('');
--- js/greeting.js
+++ js/greeting.js
@@ -17,7 +17,7 @@ function onLoginSubmit(event) {
 }
 
 function paintGreetings(username) {
-    greeting.innerText = `Hello ${username}`;
+    greeting.innerText = `Hello ${username}!`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
     logoutBtn.classList.remove(HIDDEN_CLASSNAME);
     // toDoForm.classList.remove(HIDDEN_CLASSNAME);
