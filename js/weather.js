76c9cdbec080246ad726278278f2c0bbbcd94c4a
CodingGuyJW <86402538+CodingGuyJW@users.noreply.github.com>
2021-12-01 12:12:29 +0900

    using-google-map-API

--- js/weather.js
+++ js/weather.js
@@ -1,22 +1,29 @@
 const API_KEY = "554d326393c90b72a51903b92af13fc2";
-const KAKAO_MAP_API_KEY = "ed69e9b73d46767af2f1d9981c5347ea";
+const GOOGLE_API_KEY = "AIzaSyC5OxEAdq13B3waRMudYBvuySYjtQ_7DSc";
 
 function onGeoOk(position) {
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
-    const urlKakaoMap = ``;
+    const googleUrl = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lon}&key=${GOOGLE_API_KEY}`;
     console.log(lat,lon);
 
     fetch(url)
     .then( response => response.json() )
     .then( data => {
         const weather = document.querySelector("#weather span:first-child");
-        const city = document.querySelector("#weather span:last-child");
-        city.innerText = data.name;
         weather.innerText = `${data.weather[0].main} / ${data.main.temp.toFixed(1)}℃ / `;
     });
+
+    fetch(googleUrl)
+    .then( googleRes => googleRes.json() )
+    .then( googleData => {
+        const city = document.querySelector("#weather span:last-child");
+        // console.log(`${googleData.results[0].address_components[3].long_name}`);
+        city.innerText = `${googleData.results[0].address_components[3].long_name}`;
+    });
 }
+
 function onGeoError() {
     alert("Can't find you. No weather for you.");
 }
